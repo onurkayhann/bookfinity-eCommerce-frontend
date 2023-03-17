44aafa137b7d5ffb3e965e0ee98d7d613049d786
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-03-17 14:38:02 +0100

    add Screens and folder structure

---
+++ Screens/Books/BookContainer.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+
+const BookContainer = () => {
+  <View>
+    <Text>Book Container</Text>
+  </View>;
+};
+
+export default BookContainer;
