83ae8402583028d70c1b09a923e3db2b2456886b
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-03-20 11:56:11 +0100

    test

--- Screens/Books/BookContainer.js
+++ Screens/Books/BookContainer.js
@@ -1,10 +1,38 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
+
+const data = require('../../assets/data/books.json');
 
 const BookContainer = () => {
-  <View>
-    <Text>Book Container</Text>
-  </View>;
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    setBooks(data);
+
+    return () => {
+      setBooks([]);
+    };
+  }, []);
+
+  return (
+    <View>
+      <Text>Book Container</Text>
+      <View style={{marginTop: 100}}>
+        <FlatList
+          horizontal
+          data={books}
+          renderItem={({ item }) => <Text>{item.name}</Text>}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+    </View>
+  );
 };
 
 export default BookContainer;
