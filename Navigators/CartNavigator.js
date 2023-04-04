5d51737c36689ad6867ea9018cf05a24c5968f1d
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-04-04 18:37:42 +0200

    npm install @react-navigation/material-top-tabs, create Checkout, Payment, Confirm and Checkout Navigator

--- Navigators/CartNavigator.js
+++ Navigators/CartNavigator.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Cart from '../Screens/Cart/Cart';
-// import Checkout
+import CheckoutNavigator from './CheckoutNavigator';
 
 const Stack = createStackNavigator();
 
@@ -13,6 +13,13 @@ function MyStack() {
         name='Cart'
         component={Cart}
         options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='Checkout'
+        component={CheckoutNavigator}
+        options={{
+          title: 'Checkout',
+        }}
       />
     </Stack.Navigator>
   );
