611e6092133e4f8b3a01ac5bb55025c16097944a
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-04-05 10:42:10 +0200

    add Form folder, create FormContainer and Input for shipping purposes. FormContainer is the container for the input

--- Screens/Cart/Checkout/Checkout.js
+++ Screens/Cart/Checkout/Checkout.js
@@ -3,6 +3,10 @@ import { Text, View } from 'react-native';
 
 import { connect } from 'react-redux';
 
+// Form & Input
+import FormContainer from '../../../Shared/Form/FormContainer';
+import Input from '../../../Shared/Form/Input';
+
 const Checkout = () => {
   return (
     <View>
