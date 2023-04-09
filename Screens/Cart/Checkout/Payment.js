231f713ba8b9e30fb9db36ed20479bef931fdbd9
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-04-09 14:27:45 +0200

    add array of paymentMethods, array of paymentCards, import necessary things from native-base and react-native, create useState and create container filled with options to choose for payment, create button that navigates the user to Confirm page

--- Screens/Cart/Checkout/Payment.js
+++ Screens/Cart/Checkout/Payment.js
@@ -1,11 +1,87 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button } from 'react-native';
+import {
+  Container,
+  Header,
+  Content,
+  ListItem,
+  Text,
+  Radio,
+  Right,
+  Left,
+  Picker,
+  Icon,
+  Body,
+  Title,
+} from 'native-base';
 
-const Payment = () => {
+// Payment method
+const paymentMethods = [
+  { name: 'Cash on Delivery', value: 1 },
+  { name: 'Bank Transfer', value: 2 },
+  { name: 'Card Payment', value: 3 },
+];
+
+// Payment method cards
+const paymentCards = [
+  { name: 'Wallet', value: 1 },
+  { name: 'Visa', value: 2 },
+  { name: 'MasterCard', value: 3 },
+  { name: 'Other', value: 4 },
+];
+
+const Payment = (props) => {
+  const [selectedMethod, setSelectedMethod] = useState();
+  const [card, setCard] = useState();
+
+  const order = props.route.params;
+
   return (
-    <View>
-      <Text>Payment Screen</Text>
-    </View>
+    <Container>
+      <Header>
+        <Body>
+          <Title>Choose payment method</Title>
+        </Body>
+      </Header>
+      <Content>
+        {paymentMethods.map((item, i) => {
+          return (
+            <ListItem
+              key={item.name}
+              onPress={() => setSelectedMethod(item.value)}
+            >
+              <Left>
+                <Text>{item.name}</Text>
+              </Left>
+              <Right>
+                <Radio selected={selectedMethod == item.value} />
+              </Right>
+            </ListItem>
+          );
+        })}
+        {selectedMethod == 3 ? (
+          <Picker
+            mode='dropdown'
+            iosIcon={<Icon name='arrow-down' />}
+            headerStyle={{ backgroundColor: '#d00355' }}
+            headerBackButtonTextStyle={{ color: '#fff' }}
+            headerTitleStyle={{ color: '#fff' }}
+            selectedValue={card}
+            onValueChange={(value) => setCard(value)}
+          >
+            {paymentCards.map((c, i) => {
+              return <Picker.Item key={c.name} label={c.name} value={c.name} />;
+            })}
+          </Picker>
+        ) : null}
+        <View style={{ marginTop: 60, alignSelf: 'center' }}>
+          <Button
+            title={'Submit'}
+            onPress={() => props.navigation.navigate('Confirm', { order })}
+          />
+        </View>
+      </Content>
+    </Container>
   );
 };
 
