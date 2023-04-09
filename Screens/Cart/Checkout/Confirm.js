df9377f595423b895c6eb62fd7eccc242993c183
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-04-09 20:10:46 +0200

    add buyed item info, add and connect Confirm to redux

--- Screens/Cart/Checkout/Confirm.js
+++ Screens/Cart/Checkout/Confirm.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { View, StyleSheet, Dimensions, ScrollView } from 'react-native';
+import { View, StyleSheet, Dimensions, ScrollView, Button } from 'react-native';
 import { Text, Left, Right, ListItem, Thumbnail, Body } from 'native-base';
 
 // Redux
 import { connect } from 'react-redux';
 import * as actions from '../../../Redux/Actions/cartActions';
 
-var { height } = Dimensions.get('window');
+var { height, width } = Dimensions.get('window');
 
 const Confirm = (props) => {
+  const submitOrder = () => {
+    
+    setTimeout(() => {
+      props.clearCart();
+      props.navigation.navigate('Cart')
+    }, 500)
+  }
   const submit = props.route.params;
 
   return (
@@ -19,18 +26,49 @@ const Confirm = (props) => {
       {props.route.params ? (
         <View style={{ borderWidth: 1, borderColor: '#d00355' }}>
           <Text style={styles.shipping}>Shipping to:</Text>
-          <View style={{ padding: 8, alignSelf: 'center' }}>
+          <View style={{ padding: 8 }}>
             <Text>Address: {submit.order.order.shippingAddress}</Text>
             <Text>Address 2: {submit.order.order.shippingAddress2}</Text>
             <Text>City: {submit.order.order.city}</Text>
             <Text>Zip code: {submit.order.order.zipCode}</Text>
             <Text>Country: {submit.order.order.country}</Text>
           </View>
+          <Text style={styles.shipping}>Items:</Text>
+          {submit.order.order.orderItems.map((i) => {
+            return (
+              <ListItem
+              key={i.book.name}
+              style={styles.orderedItems}
+              avatar
+              >
+                <Left>
+                  <Thumbnail source= {{uri: i.book.image}} />
+                </Left>
+                <Body style={styles.body}>
+                <Left>
+                  <Text>{i.book.name}</Text>
+                </Left>
+                <Right>
+                  <Text>${i.book.price}</Text>
+                </Right>
+                </Body>
+              </ListItem>
+            )
+          })}
         </View>
       ) : null}
+      <View style={{alignItems: 'center', margin: 20}}>
+        <Button title={'Submit order'} onPress={submitOrder} />
+      </View>
     </ScrollView>
   );
 };
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    clearCart: () => dispatch(actions.clearCart())
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -50,6 +88,17 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
   },
+  orderedItems: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    width: width / 1.2
+  },
+  body: {
+    margin: 10,
+    alignItems: 'center',
+    flexDirection: 'row'
+  }
 });
 
-export default Confirm;
+export default connect(null, mapDispatchToProps)(Confirm);
