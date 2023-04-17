e9a76b6c79578312a081558aa75f8dcc1be131da
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-04-18 00:13:24 +0200

    create stack and create components: Login, Register, UserProfile, create UserNavigator and import it in Main

--- Navigators/Main.js
+++ Navigators/Main.js
@@ -6,6 +6,7 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 // Stacks
 import HomeNavigator from './HomeNavigator';
 import CartNavigator from './CartNavigator';
+import UserNavigator from './UserNavigator';
 
 import CartIcon from '../Shared/CartIcon';
 
@@ -59,7 +60,7 @@ const Main = () => {
       />
       <Tab.Screen
         name='User'
-        component={HomeNavigator}
+        component={UserNavigator}
         options={{
           tabBarIcon: ({ color }) => (
             <Icon name='user' color={color} size={30} />
