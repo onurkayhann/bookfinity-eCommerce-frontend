c5c796e561bc9e662bb17f6e708928432d3a1f51
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-04-20 01:04:41 +0200

    create UserProfile component

--- Screens/User/UserProfile.js
+++ Screens/User/UserProfile.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Button, StyleSheet } from 'react-native';
+import { Container } from 'native-base';
 
-const UserProfile = (props) => {
+const UserProfile = () => {
   return (
-    <View>
-      <Text>User Profile Screen</Text>
-    </View>
+    <Container style={styles.container}>
+      <ScrollView contentContainerStyle={styles.subContainer}>
+        <Text style={{ fontSize: 30 }}>Dear Biketrace customer!</Text>
+        <View style={{ marginTop: 20 }}>
+          <Text style={{ margin: 10 }}>Email: Your email here</Text>
+          <Text style={{ margin: 10 }}>Phone: your phone here</Text>
+        </View>
+        <View style={{ marginTop: 80 }}>
+          <Button title={'Sign Out'} />
+        </View>
+      </ScrollView>
+    </Container>
   );
 };
+
+// Render name, email and phone dynamically
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  subContainer: {
+    alignItems: 'center',
+    marginTop: 60,
+  },
+});
 
 export default UserProfile;
