0da84ef4e61dee3c4d7a39190cc6b451e5643b53
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-04-20 00:04:27 +0200

    finalize login

--- Screens/User/Login.js
+++ Screens/User/Login.js
@@ -5,13 +5,14 @@ import FormContainer from '../../Shared/Form/FormContainer';
 import Input from '../../Shared/Form/Input';
 import Error from '../../Shared/Error';
 
-const Login = (props) => {
+import baseURL from '../../assets/common/baseURL';
 
+const Login = (props) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     const user = {
       email,
       password,
@@ -20,7 +21,27 @@ const Login = (props) => {
     if (email === '' || password === '') {
       setError('Please fill in your credentials');
     } else {
-      console.log('Logout');
+      try {
+        const response = await fetch(`${baseURL}users/login`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(user),
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+          console.log(data); // do something with the user data
+          props.navigation.navigate('User Profile');
+        } else {
+          setError(data.message);
+        }
+      } catch (err) {
+        console.log(err);
+        setError('An error occurred. Please try again later.');
+      }
     }
   };
 
